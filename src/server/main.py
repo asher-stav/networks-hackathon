ac901564c50d861bf66183bb23f359d28cd92182
asher-stav <asherst@post.bgu.ac.il>
2025-01-11 17:51:48 +0200

    Add offer sending mechanism to server

--- src/server/main.py
+++ src/server/main.py
@@ -4,21 +4,24 @@ from server import Server
 
 UDP_PORT_INDEX = 1
 TCP_PORT_INDEX = 2
+BROADCAST_PORT_INDEX = 3
 
 
 def main() -> None:
     udp_port: int
     tcp_port: int
+    broadcast_port: int
 
     try:
         udp_port = int(sys.argv[UDP_PORT_INDEX])
         tcp_port = int(sys.argv[TCP_PORT_INDEX])
+        broadcast_port = int(sys.argv[BROADCAST_PORT_INDEX])
     except Exception:
-        print("Invalid command line arguments. Format: <udp_port> <tcp_port>")
+        print("Invalid command line arguments. Format: <udp_port> <tcp_port> <broadcast_port>")
         return
     
     # Get the port numbers from the user
-    server: Server = Server(udp_port, tcp_port)
+    server: Server = Server(udp_port, tcp_port, broadcast_port)
 
 if __name__ == '__main__':
     main()
--- src/server/server.py
+++ src/server/server.py
@@ -1,6 +1,8 @@
+from logging import shutdown
 import socket
 import struct
 import threading
+import time
 
 
 COOKIE = 0xabcddcba
@@ -8,6 +10,7 @@ UDP_OFFER_MSG_CODE = 0x2
 UDP_REQUEST_MSG_CODE = 0x3
 UDP_PAYLOAD_MSG_CODE = 0x4
 
+BROADCAST_IP = '255.255.255.255'
 
 class Server:
     """
@@ -17,17 +20,32 @@ class Server:
     __shutdown: bool
     __udp_port: int
     __tcp_port: int
+    __broadcast_port: int
+    __shutdown_lock: threading.Lock
     
-    def __init__(self, udp_port: int, tcp_port: int):
+    def __init__(self, udp_port: int, tcp_port: int, broadcast_port: int):
         self.__shutdown = False
         self.__udp_port = udp_port
         self.__tcp_port = tcp_port
+        self.__broadcast_port = broadcast_port
+        self.__shutdown_lock = threading.Lock()
+
+    def is_shutdown(self) -> bool:
+        shutdown: bool
+        self.__shutdown_lock.acquire()
+        shutdown = self.__shutdown
+        self.__shutdown_lock.release()
+
+        return shutdown
     
     def listening(self):
         ip: str = ''
-        offer_sock: socket.socket = None
         # TODO - start listening to clients
-        print(f'Server started, listening on IP address {'TODO'}') 
+        offer_sock: socket.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        offer_thread: threading.Thread = threading.Thread(target=self.announcer, args=(offer_sock, ))
+        offer_thread.start()
+
+        print(f'Server started, listening on IP address {'TODO'}')
         self.announcer(offer_sock)
 
     def shutdown(self) -> None:
@@ -37,10 +55,27 @@ class Server:
         """
         pass        
             
-    def announcer(self, sock: socket.socket):
-        while not self.__shutdown:
-            # TODO - send offers every 1 second.
-            pass
+    def announcer(self, sock: socket.socket) -> None:
+        """
+        Sends offers to connect to the server every second
+        """
+        while not self.is_shutdown():
+            self.send_upd_offer(sock)
+            time.sleep(1)
+
+    def send_upd_offer(self, sock: socket.socket) -> None:
+        """
+        Sends an broadcast offer to connect to the server.
+        Message structure:
+            - magic cookie, 4 bytes
+            - message code, 2 bytes
+            - server UDP port, 2 bytes
+            - server TCP port, 2 bytes
+        """
+        message: bytes
+        message = struct.pack('>I', COOKIE)
+        message += struct.pack('>H', UDP_OFFER_MSG_CODE, self.__udp_port, self.__tcp_port)
+        sock.sendto(message, (BROADCAST_IP, self.__broadcast_port))
         
     def listen_udp(self):
         pass
