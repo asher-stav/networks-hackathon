ddcfd40d395abe01bd505804ae481b7182b02501
asher-stav <asherst@post.bgu.ac.il>
2025-01-11 10:33:44 +0200

    Added informative comments to client/Client and slight structure change

--- src/client/client.py
+++ src/client/client.py
@@ -3,35 +3,48 @@ import struct
 import threading
 
 class Client:
-    shutdown: bool = False
-    data_size: int
-    tcp_connections_num: int
-    udp_connections_num: int
-
+    """
+    A class representing a client that can connect to servers and run speed-tests on them, both with UDP and TCP connections.
+    In addition, will collect interesting data about the servers and the connections.
+    """
+    __shutdown: bool
+    __data_size: int
+    __tcp_connections_num: int
+    __udp_connections_num: int
     
     def __init__(self, data_size: int, tcp_connections_num: int, udp_connections_num: int):
-        self.data_size = data_size
-        self.tcp_connections_num = tcp_connections_num
-        self.udp_connections_num = udp_connections_num
-        
-    # Runs the client until stopped, constntly looking for servers to run a speedtest on.
-    def run(self):
-        while(not self.shutdown):
-            sock : socket
-            # TODO - Look for a server, and connect to it
-            self.request_file(sock)
+        self.__shutdown = False
+        self.__data_size = data_size
+        self.__tcp_connections_num = tcp_connections_num
+        self.__udp_connections_num = udp_connections_num
     
-    def request_file(self, sock: socket):
-        for i in range(self.tcp_connections_num):
+    def run(self) -> None:
+        """
+        Runs the client until stopped, constantly looking for servers to run a speedtest on.
+        """
+        while not self.__shutdown:
+            sock: socket.socket = None
+            # TODO - Look for a server
+            self.request_file()
+
+    def shutdown(self) -> None:
+        """
+        Shuts down the client, preventing it from receiving further offers.
+        If the client runs a task currently, it will finish it before termination.
+        """
+        pass
+    
+    def request_file(self, server_addr: str, server_udp_port: int, server_tcp_port: int) -> None:
+        for i in range(self.__tcp_connections_num):
             # TODO - Create a new thread for each tcp connection with the method tcp_connect
             pass
-        for i in range(self.udp_connections_num):
+        for i in range(self.__udp_connections_num):
             # TODO - Create a new thread for each udp connection with the method udp_connect
             pass
         pass
 
-    def tcp_connect():
+    def tcp_connect(self, server_addr: str, server_tcp_port: int) -> None:
         pass
     
-    def udp_conenct():
+    def udp_connect(self, server_addr: str, server_udp_port: int) -> None:
         pass
