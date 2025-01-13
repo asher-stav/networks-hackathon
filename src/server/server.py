f2f1749d82bf13a3ba823a8cd14e0de49cfd7189
asher-stav <asherst@post.bgu.ac.il>
2025-01-13 16:46:57 +0200

    Add handling of TCP and UDP clients

--- src/server/server.py
+++ src/server/server.py
@@ -1,3 +1,4 @@
+from email import message
 from logging import shutdown
 import socket
 import struct
@@ -10,9 +11,13 @@ UDP_OFFER_MSG_CODE = 0x2
 UDP_REQUEST_MSG_CODE = 0x3
 UDP_PAYLOAD_MSG_CODE = 0x4
 
+REQUEST_MESSAGE_LEN = 13
+REQUEST_COOKIE_INDEX = 0
+REQUEST_TYPE_INDEX = 4
+REQUEST_FILE_SIZE_INDEX = 5
+
 BROADCAST_IP = '255.255.255.255'
 
-REQUEST_MESSAGE_LEN = 13
 
 class Server:
     """
@@ -95,7 +100,8 @@ class Server:
         """
         message: bytes
         message = struct.pack('>I', COOKIE)
-        message += struct.pack('>H', UDP_OFFER_MSG_CODE, self.__udp_port, self.__tcp_port)
+        message += struct.pack('>B', UDP_OFFER_MSG_CODE)
+        message += struct.pack('>H', self.__udp_port, self.__tcp_port)
         self.__offer_sock.sendto(message, (BROADCAST_IP, self.__broadcast_port))
         
     def listen_udp(self):
@@ -110,8 +116,56 @@ class Server:
             client_sock: socket.socket = self.__tcp_sock.accept()
             threading.Thread(target=self.handle_tcp_connection, args=(client_sock, )).start()
 
-    def handle_udp_connection(self, data: bytes, address):
+    def handle_udp_connection(self, data: bytes, address) -> None:
+        cookie: int = struct.unpack('>I', data[REQUEST_COOKIE_INDEX:REQUEST_TYPE_INDEX])
+        if cookie != COOKIE:
+            print(f'Error: received invalid cookie: {cookie}')
+            return
+        
+        message_type: int = struct.unpack('>B', data[REQUEST_TYPE_INDEX:REQUEST_FILE_SIZE_INDEX])
+        if message_type != UDP_REQUEST_MSG_CODE:
+            print(f'Error: received invalid message type: {message_type}. Expected: {UDP_REQUEST_MSG_CODE}')
+            return
+        
+        file_size: int = struct.unpack('>L', data[REQUEST_FILE_SIZE_INDEX:REQUEST_MESSAGE_LEN])
+
+        segments_amount: int = Server.get_udp_segments_amount(file_size)
+        single_segment_payload: int = file_size // segments_amount
+        message_start: bytes = struct.pack('>I', COOKIE) + struct.pack('>B', UDP_PAYLOAD_MSG_CODE) + \
+            struct.pack('>L', segments_amount)
+
+        for curr_segment in range(segments_amount):
+            message: bytes = b''
+            message += message_start
+            message += struct.pack('>L', curr_segment)
+            message += ('a' * single_segment_payload).encode()
+            try:
+                self.__udp_sock.sendto(message, address)
+            except Exception:
+                print(f'Error: failed to send segment number {curr_segment} to udp client!')
+
+    @staticmethod
+    def get_udp_segments_amount(file_size: int) -> int:
+        """
+        TODO: return number of segments to send based on file size
+        """
         pass
 
-    def handle_tcp_connection(self, client_sock: socket.socket):
-        pass
+    def handle_tcp_connection(self, client_sock: socket.socket) -> None:
+        bytes_amount: int = 0
+        curr_char: str = ''
+        while curr_char != '\n':
+            curr_char = client_sock.recv(1).decode()
+            if not curr_char.isdigit():
+                print('Error: received a non-digit char from client in TCP connection!')
+                client_sock.close()
+                return
+            bytes_amount = (10 * bytes_amount) + int(curr_char)
+
+        for i in range(bytes_amount):
+            try:
+                client_sock.send('a')
+            except Exception:
+                print(f'Error: failed to send byte number {i} to tcp client!')
+                break
+        client_sock.close()
