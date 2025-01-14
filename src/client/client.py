ee0bc8e80ba0995c51eec14fc0cb762cb5ed26cb
Mendy <dishonme@post.bgu.ac.il>
2025-01-14 10:30:51 +0200

    implemented udp_connect, fixed some constants.

--- src/client/client.py
+++ src/client/client.py
@@ -1,17 +1,20 @@
+from pickletools import int4
 import socket
 import struct
 import threading
+import time
 
+BYTE_SIZE = 8
 
 # message structure constants:
 COOKIE = 0xabcddcba
-COOKIE_INDEX = 0
+COOKIE_IDX = 0
 COOKIE_LEN = 4
 
 TYPE_OFFER = 0x2
 TYPE_REQUEST = 0x3
 TYPE_PAYLOAD = 0x4
-TYPE_INDEX = 4
+TYPE_IDX = 4
 TYPE_LEN = 1
 # OFFER message
 OFFER_UDP_IDX = 5
@@ -22,15 +25,16 @@ OFFER_MSG_LEN = 9
 # REQUEST message
 REQUEST_FILE_SIZE_IDX = 5
 REQUEST_FILE_LEN = 8
-REQUEST_MESSAGE_LEN = 13
+REQUEST_MSG_LEN = 13
 # PAYLOAD message
 PAYLOAD_SEGMENT_COUNT_IDX = 5
 PAYLOAD_SEGMENT_COUNT_LEN = 8
 PAYLOAD_CURRENT_SEGMENT_IDX = 13
 PAYLOAD_CURRENT_SEGMENT_LEN = 8
+PAYLOAD_DATA_IDX = 21
 # actual payload length is unknown, will be calculated in runtime.
+MAX_PAYLOAD_MSG_SIZE = 1024
 
-BROADCAST_IP = '255.255.255.255'
 
 class Client:
     """
@@ -39,14 +43,14 @@ class Client:
     """
     __port: int
     __shutdown: bool
-    __data_size: int
+    __data_size: bytes
     __tcp_connections_num: int
     __udp_connections_num: int
     
     def __init__(self, port: int, data_size: int, tcp_connections_num: int, udp_connections_num: int):
         self.__port = port
         self.__shutdown = False
-        self.__data_size = data_size
+        self.__data_size = struct.pack("Q", data_size)
         self.__tcp_connections_num = tcp_connections_num
         self.__udp_connections_num = udp_connections_num
     
@@ -73,8 +77,8 @@ class Client:
             data, addr = sock.recvfrom(OFFER_MSG_LEN + 1)
             # check for correct offer message structure and values.
             if(len(data) == OFFER_MSG_LEN &
-                data[COOKIE_INDEX:COOKIE_INDEX+COOKIE_LEN] == COOKIE &
-                  data[TYPE_INDEX] == TYPE_OFFER):
+                data[COOKIE_IDX:COOKIE_IDX+COOKIE_LEN] == COOKIE &
+                  data[TYPE_IDX] == TYPE_OFFER):
                 # H for unsigned short (2 bytes)
                 # < for little endian
                 self.request_file(addr[0],
@@ -100,4 +104,32 @@ class Client:
         pass
     
     def udp_connect(self, server_addr: str, server_udp_port: int) -> None:
-        pass
+        sock: socket.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        addr: tuple[str, int] = (server_addr, server_udp_port)
+        request_msg: bytes = struct.pack(COOKIE, TYPE_REQUEST, self.__data_size)
+        req_data_size: int = struct.unpack("Q", self.__data_size)[0]
+        data_left: int = req_data_size
+
+        start_time: float = time.time()
+        sock.sendto(request_msg, addr)
+        try:
+            while(data_left > 0):
+                data, server = sock.recvfrom(MAX_PAYLOAD_MSG_SIZE)
+                if(data[COOKIE_IDX] != COOKIE & data[TYPE_IDX] != TYPE_PAYLOAD):
+                    print ("Received a non-payload message from the server.")
+                    return
+                # calculate the actual payload length
+                payload_len: int = len(data[PAYLOAD_DATA_IDX:])
+                data_left -= payload_len
+            
+            end_time: float = time.time()
+            transfer_time: float = end_time - start_time
+            transfer_rate: float = BYTE_SIZE*req_data_size / transfer_time
+            print ("UDP connection to server {}:{} finished. Transfer rate: {} bits/sec."
+                   .format(server_addr, server_udp_port, transfer_rate))
+
+        except socket.timeout:
+            print ("UDP connection to server {}:{} timed out.".format(server_addr, server_udp_port))
+            return
+
+        
